6d3de877bc50561d35e6278388005663f71259c8
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-11-30 18:58:26 +0100

    implemented fetch using the api to display list of characters by name

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const characters = ref([]);
+const loading = ref(true);
+const error = ref(null);
+
+onMounted(async () => {
+  try {
+    const response = await fetch(" https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    characters.value = data.results;
+  } catch (err) {
+    error.value = "Failed to fetch data";
+  } finally {
+    loading.value = false;
+  }
+});
+
+console.log(characters);
+</script>
 
 <template>
-  <h1>Rick and Morty</h1>
+  <div>
+    <h1>Rick and Morty</h1>
+    <h2>characters</h2>
+    <ul>
+      <li v-for="character in characters" :key="character.id">
+        <p>{{ character.name }}</p>
+      </li>
+    </ul>
+    <p v-if="loading">Loading...</p>
+    <p v-if="error" class="error">{{ error }}</p>
+  </div>
 </template>
 
 <style scoped></style>
