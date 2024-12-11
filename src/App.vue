029f050424152c43068569c07d093bdcbba15ff9
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-12-11 19:00:53 +0100

    updated App.vue to display characters list as cards

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue";
+import Card from "./components/Card.vue";
 
 const characters = ref([]);
 const loading = ref(true);
@@ -24,14 +25,31 @@ console.log(characters);
   <div>
     <h1>Rick and Morty</h1>
     <h2>characters</h2>
-    <ul>
-      <li v-for="character in characters" :key="character.id">
-        <p>{{ character.name }}</p>
-      </li>
-    </ul>
     <p v-if="loading">Loading...</p>
     <p v-if="error" class="error">{{ error }}</p>
+    <div class="cards-container">
+      <Card
+        v-for="character in characters"
+        :key="character.id"
+        :name="character.name"
+        :status="character.status"
+        :gender="character.gender"
+        :image="character.image"
+      />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.cards-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 16px;
+  padding: 16px;
+}
+
+.error {
+  color: red;
+  font-weight: bold;
+}
+</style>
